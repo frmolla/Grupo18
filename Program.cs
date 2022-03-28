99912dc9adf7af54f36766f248b3cfe590e261fb
Francisco Mollá Astrar <frmolla@ucm.es>
2022-03-28 12:07:01 +0200

    Conflicto Tipo 1

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace Grupo18
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Conflicto Tipo1");
         }
     }
 }
