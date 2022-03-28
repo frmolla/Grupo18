3b9c01949f02196b7ef9ee6cce6e0193ff8a469b
Francisco Mollá Astrar <frmolla@ucm.es>
2022-03-28 12:32:36 +0200

    Modificar program.cs

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace Grupo18
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Conflicto Tipo1");            
+            Console.WriteLine("Conflicto Tipo1");
+            Console.WriteLine("Modificar archivos diferentes-Estuadiante A");
         }
     }
 }
